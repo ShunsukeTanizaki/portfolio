1f06b5e6fadf3415fd3d60ec7a8a2eae3c3873e7
ShunsukeTanizaki <s21valley@gmail.com>
2019-06-19 00:35:32 +0900

    6/18

--- index.php
+++ index.php
@@ -1,71 +1,52 @@
-<?php
-//DB接続情報
-
-$user     = 'testuser';
-$password = 'pw4testuser';
-$dbName   = 'keijiban';
-$host     = 'localhost';
-
-$dsn      = "mysql:host={$host};dbname={$dbName};charset=utf8;";
-?>
-
-<!DOCTYPE htnl>
+<!DOCTYPE html>
 <html lang="ja">
 <head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="css/style.css" type="text/css"media="all">
-<title>掲示板</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>掲示板</title>
+
+	<link rel="stylesheet" href="style.css" />
 </head>
+
 <body>
-　<section>
-　<h2>新規投稿</h2>
-　 <?php echo $message; ?>
-　  <form method="post" action="">
-　  名前：<input type="text" name="name" value="<?php echo $name; ?>" >
-　  <?php echo $err_msg1; ?><br>
-　  紹介文：<textarea name="Introduction" rows="10" cols="100" wrap="hard" placeholder="紹介文をお書き下さい"><?php echo $comment; ?></textarea>
-　  <?php echo $err_msg2; ?><br>
-　<input type="submit" name="" value="クリック" >
-　 </form>
-　</section>
-<section>
-  <h2>投稿画面</h2>
-  <p>投稿はまだありません</p>
-　  <dl>
-　    <?php foreach( $dataArr as $data ):?>
-　    <p><span><?php echo $data["name"]; ?></span>:<span><?php echo $data["Introduction"]; ?></span></p>
-　    <?php endforeach;?>
-　  </dl>
-</section>
+<div id="wrap">
+  <div id="head">
+    <h1>掲示板</h1>
+  </div>
+  <div id="content">
+  	<!-- <div style="text-align: right"><a href="logout.php">ログアウト</a></div> -->
+    <form action="" method="post">
+      <dl>
+        <dt>文章入力</dt>
+        <dd>
+          <textarea name="message" cols="50" rows="5"></textarea>
+          <input type="hidden" name="reply_post_id" value="" />
+        </dd>
+      </dl>
+      <div>
+        <p>
+          <input type="submit" value="投稿する" />
+        </p>
+      </div>
+    </form>
 
+    <div class="msg">
+    <img src="member_picture" width="48" height="48" alt="" />
+    <!-- <p><span class="name">（）</span>[<a href="index.php?res=">Re</a>]</p>
+    <p class="day"><a href="view.php?id="></a>
+<a href="view.php?id=">
+返信元のメッセージ</a>
+[<a href="delete.php?id="
+style="color: #F33;">削除</a>] -->
+    </p>
+    </div>
 
-<div>
-
-<?php
-//MySQLデータベースに接続する
-try {
-  //DBへの接続を表すPDOインスタンスを生成
-  $pdo = new PDO($dsn, $user, $password);
-  //プリペアドステートメントのエミュレーションを無効にする
-  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARS, false);
-  //例外がスローされる設定にする
-  $pdo->setAttrebute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "データベース{$dbName}に接続しました。","<br>";
-
-  $sql = "SELECT * FROM post";
-  $stm->excuse();
-  //接続を解除する
-  $pdo = NULL;
-
-} catch(PDOException $e) {
-    echo '<span class="errer">エラーがありました。</span><br>';
-  //   echo $e->getMessage();
-  // exit();
-}
-
-?>
+<!-- <ul class="paging">
+<li><a href="index.php?page=">前のページへ</a></li>
+<li><a href="index.php?page=">次のページへ</a></li>
+</ul> -->
+  </div>
 </div>
 </body>
 </html>
-
-

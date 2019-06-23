be13c2caf0b18780e88fa8f03b92cd0c1a79335b
ShunsukeTanizaki <s21valley@gmail.com>
2019-06-23 17:16:19 +0900

    修正

--- index.php
+++ index.php
@@ -1,27 +1,10 @@
-<?php
-if (!empty($_POST)) {
-  if($_POST['message'] !== '') {
-    $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, created=NOW()');
-    $message->execute(array(
-      $member['id'],
-      $_POST['message']
-    ));
-
-    header('Location: index.php');
-    exit();
-  }
-}
-
-$posts = $db->query('SELECT m.name,m.picture,p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
-
-?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>掲示板</title>
+	<title>ひとこと掲示板</title>
 
 	<link rel="stylesheet" href="style.css" />
 </head>
@@ -29,13 +12,13 @@ $posts = $db->query('SELECT m.name,m.picture,p.* FROM members m, posts p WHERE m
 <body>
 <div id="wrap">
   <div id="head">
-    <h1>掲示板</h1>
+    <h1>ひとこと掲示板</h1>
   </div>
   <div id="content">
-  	<!-- <div style="text-align: right"><a href="logout.php">ログアウト</a></div> -->
+  	<div style="text-align: right"><a href="logout.php">ログアウト</a></div>
     <form action="" method="post">
       <dl>
-        <dt>文章入力</dt>
+        <dt>○○さん、メッセージをどうぞ</dt>
         <dd>
           <textarea name="message" cols="50" rows="5"></textarea>
           <input type="hidden" name="reply_post_id" value="" />
@@ -48,18 +31,16 @@ $posts = $db->query('SELECT m.name,m.picture,p.* FROM members m, posts p WHERE m
       </div>
     </form>
 
-<?php foreach ($posts as $post): ?>
     <div class="msg">
-    <img src="member_picture/<?php print (htmlspecialchars($post['picture'], ENT_QUOTES)); ?>" width="48" height="48" alt="<?php print (htmlspecialchars($post['name'], ENT_QUOTES)); ?>" />
-    <p><?php print (htmlspecialchars($post['message'], ENT_QUOTES)); ?><span class="name">（<?php print (htmlspecialchars($post['name'], ENT_QUOTES)); ?>）</span>[<a href="index.php?res=">Re</a>]</p>
-    <p class="day"><a href="view.php?id="><?php print (htmlspecialchars($post['created'], ENT_QUOTES)); ?></a>
+    <img src="member_picture" width="48" height="48" alt="" />
+    <p><span class="name">（）</span>[<a href="index.php?res=">Re</a>]</p>
+    <p class="day"><a href="view.php?id="></a>
 <a href="view.php?id=">
 返信元のメッセージ</a>
 [<a href="delete.php?id="
 style="color: #F33;">削除</a>]
     </p>
     </div>
-<?php endforeach; ?>
 
 <ul class="paging">
 <li><a href="index.php?page=">前のページへ</a></li>
